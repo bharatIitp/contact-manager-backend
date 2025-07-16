0db71fd0521f92a277935134a6789e6d4453837e
Bharat <bharat_2312res219@iitp.ac.in>
2025-07-16 18:20:56 +0530

    feat: Add contact routes for CRUD

--- app.js
+++ app.js
@@ -1,24 +1,31 @@
 import dotenv from 'dotenv';
-
 import express from 'express';
 import connectDB from './config/db.js';
+import contactRoutes from './routes/contactRoutes.js';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Body parser middleware (JSON)
+app.use(express.json());
 
-connectDB().then(() => {
-  app.use(express.json());
+//Routes
+app.use('/api/contacts', contactRoutes);
 
-  app.get('/health', (req, res) => {
-    res.status(200).json({ status: 'ok' });
-  });
-
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-}).catch((err) => {
-  console.error('Failed to connect to DB:', err);
+//Health Check Route
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'ok' });
 });
+
+// Start server only after DB connection
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error('Failed to connect to DB:', err);
+  });
---
+++ routes/contactRoutes.js
@@ -0,0 +1,21 @@
+import express from 'express';
+
+import {
+    getAllContacts,
+    getContactById,
+    createContact,
+    updateContact,
+    deleteContact
+} from '../controllers/contactController.js';
+
+import { Router } from 'express';
+
+const router = Router();
+
+router.get('/' , getAllContacts);
+router.get('/:id' , getContactById);
+router.post('/', createContact);
+router.put('/:id' , updateContact);
+router.delete('/:id', deleteContact);
+
+export default router;
